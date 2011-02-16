08d8fa07afe356bb11323ce325601049b6d91d2b
Caiiiyck <Caiiiyck@localhost>
2011-02-16 19:29:28 +0300

    Save last parsed line

--- log-agg/log-agg.pl
+++ log-agg/log-agg.pl
@@ -3,15 +3,17 @@
 use strict;
 use utf8;
 use File::ReadBackwards;
+use Digest::MD5 qw(md5_hex);
+use File::Copy;
 
 # =============
 # Configuration
 # =============
 
 #
-# List of log-files for look
+# Log-file to look for
 #
-my @log_files = ('/opt/production/portal/tomcat1_old/logs/catalina.out');
+my $log_file = '/opt/production/portal/tomcat1_old/logs/catalina.out';
 
 #
 # RegExp for new message in log
@@ -21,23 +23,24 @@ my @log_files = ('/opt/production/portal/tomcat1_old/logs/catalina.out');
 my $new_message = '^(\d{4,4}-\d{2,2}-\d{2,2}\s{1,1}\d{2,2}:\d{2,2}:\d{2,2},\d{1,3})\s{1,1}';
 
 #
-# 
-#
-
-#
 # Types of message to accept
 #
 my $accept_message = 'ERROR';
 
 #
-# Count of messages to display
+# Count of messages to display, 0 - unlimeted
 #
 my $max_count = 50;
 
 #
 # Target file (index.html)
 #
-my $target = '/opt/WWW/dl.4geo.ru/tmp/index.html';
+my $target = '/opt/WWW/https.w42.ru/logs/tomcat1.html';
+
+#
+# Template file
+#
+my $template_file = "template.html";
 
 # ================
 # Function`s scope
@@ -46,20 +49,14 @@ my $target = '/opt/WWW/dl.4geo.ru/tmp/index.html';
 sub prepareMessage {
     my ($template, $error_full, $count) = @_;
 
-    if ($error_full =~ /$accept_message/) {
-#	$error_full =~ s/\n/<br\/>/ig;
-
-	if ($error_full =~ $new_message) {
-	    my $time = "$1 ($count)";
-	    $template =~ s/\$error-time/$time/ig;
-	}
-
-	$template =~ s/\$error-full/$error_full/ig;
-	
-	return $template;
+    if ($error_full =~ $new_message) {
+	my $time = "$1 ($count)";
+	$template =~ s/\$error-time/$time/ig;
     }
 
-    return "";
+    $template =~ s/\$error-full/$error_full/ig;
+	
+    return $template;
 }
 
 # ============
@@ -67,43 +64,92 @@ sub prepareMessage {
 # ============
 
 #
+# Read previus md5 if exsists
+#
+my $last_md5;
+
+if (-e $target) {
+    open(INDEX, "$target") || die $!;
+    binmode(INDEX, ":utf8");
+
+    $_ = <INDEX>;
+    /<!--MD5:(.*)-->/i;
+    $last_md5 = $1;
+    
+    close(INDEX);
+}
+
+#
 # Read log files into messages hash
 #
+my $file = File::ReadBackwards->new("$log_file") || die "can't read file '$log_file': $!\n";
+
 my %messages = ();
+my $message = "";
+my $md5;
 
-foreach my $log (@log_files) {
-    my $file = File::ReadBackwards->new("$log") || die "can't read file '$log': $!\n";
+while (defined($_ = $file->readline)) {
+    $md5 = md5_hex($_) if (!$md5);
+    last if ($last_md5 && md5_hex($_) eq $last_md5);
 
-    my $message = "";
-    while (defined($_ = $file->readline)) {
-	$message = $_ . $message;
+    $message = $_ . $message;
 
-	if (/$new_message/i) {
+    if (/$new_message/i) {
+	if ($message =~ /$accept_message/i) {
 	    $messages{$message} = 0 if (!exists $messages{$message});
 	    $messages{$message}++;
 
-	    last if (scalar(values(%messages)) > $max_count);
-	    
-	    $message = "";
+	    last if ($max_count && scalar(values(%messages)) > $max_count);
 	}
+	
+	$message = "";
     }
+}
 
-    $file->close();
+$file->close();
+
+#
+# Make backup of previus $target
+#
+my $target_backup = "#";
+
+if (-e $target) {
+    if (scalar(values(%messages)) == 0) {
+	#nothing to do (up to date)
+	print "$target is up to date \n";
+	exit 0;
+    }
+    
+    $target_backup = $target;
+    $target_backup =~ s/\.html$//i;
+    $target_backup = $target_backup ."_". $last_md5 . ".html";
+
+    print $target, "->", $target_backup, "\n";
+    copy($target, $target_backup) or die "Copy failed: $!";
+
+    $target_backup =~ s/^.*\///i;
 }
 
 #
 # Open resources
 #
-open(TEMPLATE, "template.html") || die $!;
+open(TEMPLATE, "$template_file") || die $!;
 binmode(TEMPLATE, ":utf8");
 
 open(INDEX, ">$target") || die $!;
 binmode(INDEX, ":utf8");
 
 #
+# Write md5 of position in log file
+#
+print INDEX "<!--MD5:$md5-->\n";
+
+#
 # Write header
 #
 while (<TEMPLATE>) {
+    s/<!-- OLDER -->/$target_backup/ig;
+
     last if (/<!-- MESSAGE START -->/);
     print INDEX;
 }
@@ -121,14 +167,14 @@ while (<TEMPLATE>) {
 # Write messages
 #
 foreach my $message (keys %messages) {
-    my $prepared = prepareMessage($template, $message, $messages{$message});
-    print INDEX $prepared if ($prepared);
+    print INDEX prepareMessage($template, $message, $messages{$message});
 }
 
 #
 # Write footer
 #
 while (<TEMPLATE>) {
+    s/<!-- OLDER -->/$target_backup/ig;
     print INDEX;
 }
 
@@ -137,3 +183,8 @@ while (<TEMPLATE>) {
 #
 close(INDEX);
 close(TEMPLATE);
+
+#
+# Success info
+#
+print "$target was created\n";
